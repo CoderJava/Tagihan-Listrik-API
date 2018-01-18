ef756d598ef6f89840671fdbacbff4355552e53e
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-01-19 01:30:36 +0700

    Create file IndexController

--- src/main/java/com/ysn/tagihanlistrik/controller/index/IndexController.java
+++ src/main/java/com/ysn/tagihanlistrik/controller/index/IndexController.java
@@ -1,13 +1,21 @@
 package com.ysn.tagihanlistrik.controller.index;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class IndexController {
 
     @RequestMapping("/")
-    public String index() {
-        return "Spring Boot running well";
+    public ResponseEntity<Map<String, Object>> index() {
+        Map<String, Object> mapDataReturn = new HashMap<>();
+        mapDataReturn.put("success", true);
+        mapDataReturn.put("message", "Spring Boot running well");
+        return new ResponseEntity<>(mapDataReturn, HttpStatus.OK);
     }
 }
